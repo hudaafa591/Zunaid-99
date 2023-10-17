1469cc217d89716dc93962f7156d848c790467e0
Zunaid Ahmed <147224244+hudaafa591@users.noreply.github.com>
2023-10-17 18:13:54 +0600

    Create Update FILE-DUMP.py

---
+++ Update FILE-DUMP.py
@@ -0,0 +1,11 @@
+import os,time,platform
+#os.system('clear')
+#print('[>] Checking Updates')
+#os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import FILE64
+elif bit=='32bit':
+    import FILE32
+else:
+    print('\033[1;31m[×] Internet Problem')
